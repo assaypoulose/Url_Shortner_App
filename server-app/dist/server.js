4957b0ba00f17132e2ed37542033435bca7c3573
Assay Poulose <94008297+assaypoulose@users.noreply.github.com>
2024-04-22 10:08:45 +0200

    removed dist file

--- server-app/dist/server.js
+++ server-app/dist/server.js
@@ -5,12 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const dbConfig_1 = __importDefault(require("./config/dbConfig"));
+const shortUrl_route_1 = __importDefault(require("./routes/shortUrl.route"));
 dotenv_1.default.config();
+(0, dbConfig_1.default)();
 const PORT = process.env.PORT || 5001;
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)({
+    origin: "http://localhost:3000",
+    credentials: true,
+}));
+app.use("/api/", shortUrl_route_1.default);
 app.listen(PORT, () => {
     console.log(`server running on port: ${PORT}`);
 });
